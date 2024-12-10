65d97956dffbd30a75aebae802aadef7576c23a8
Romka899 <romka.compass@gmail.com>
2024-12-10 11:59:54 +0500

    last commit

--- myapp/App.js
+++ myapp/App.js
@@ -1,17 +1,26 @@
+import express from 'express';
+import * as fs from "fs";
 
-const express = require('express');
 const app = express();
 const port = 3000;
-//const v1Router = require('./getShips');
+//const v1Router = require('./getShips/index');
 
 
-/*
-app.get('/', (req, res, next) =>{
-    res.send('api');
+
+
+app.get('/api/getShips', (req, res, next) =>{
+    fs.readFile('ships.json', (err, data) =>{
+        const b = JSON.parse(data);
+        res.send(b);
+        return res;
+    })
+});
+
+app.listen(port, () =>{
+    console.log(`Example app listening on port (http://localhost:${port}/api/getShips)`)
 })
-*/
 
-app.use('/api/getShips', v1Router);
+//app.use('/api/getShips', v1Router);
 
 /*
 app.get('/v1Router', (req, res)=>{
@@ -26,6 +35,3 @@ app.post('/', (req, res) =>{
 })
 */
 
-app.listen(port, () =>{
-    console.log(`Example app listening on port (http://localhost:${port}/api/getShips)`)
-})
--- myapp/getShips/index.js
+++ myapp/getShips/index.js
@@ -1,13 +1,49 @@
-const fs = require('fs');
+import express from 'express';
+import ships from '../../ships.json';
+import * as fs from "fs";
 
+const app2 = express();
+//const v1Router = require('./ships.json');
 
-const express = require('express');
-const router = express.Router();
-
-
-
+/*
 router.route('/ships.json').get((req, res) => {
     res.send(`${req.baseUrl}`);
 });
+*/
+
+
+
+
+
+app2.get('/', (req, res, next) =>{
+    fs.readFile('ships.json', (err, data) =>{
+        const b = JSON.parse(data);
+        res.send(b);
+        return res;
+    })
+});
+
+
+/*
+router.get('/', (req, res) => {
+    res.send('Get all workouts');
+});
+
+router.get('/:workoutId', (req, res) => {
+    res.send('Get an existing workout');
+});
+
+router.post('/', (req, res) => {
+    res.send('Create a new workout');
+});
+
+router.patch('/:workoutId', (req, res) => {
+    res.send('Update an existing workout');
+});
+
+router.delete('/:workoutId', (req, res) => {
+    res.send('Delete an existing workout');
+});
 
 module.exports = router;
+*/
